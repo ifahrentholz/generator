8292b019fc1320e1a2aaa383cc752808dc4bdeeb
Ingo Fahrentholz <ingof.work@gmail.com>
2017-02-11 19:26:12 +0100

    WIP

--- blueprints/bob.config.js
+++ blueprints/bob.config.js
@@ -1,23 +1,60 @@
+/**
+ * ============================================================================
+ * DESCRIPTION:
+ * ============================================================================
+ *
+ * You can specify sources for each file if it's different
+ * from the default value.
+ * Default value would be the singularized string of the folderStructure key.
+ *
+ * If no source could be found an empty file will be created.
+ *
+ *
+ * ============================================================================
+ * EXAMPLE:
+ * ============================================================================
+ *
+ * folderStructure: {
+ *   components: {
+ *     files: [{
+ *       file: "%cName%.spec.js",
+ *       source: "component/template.spec.js"
+ *     }, {
+ *       file: "%cName%.js",
+ *       source: "component/template.js"
+ *     }, {
+ *       file: "%cName%.hbs",
+ *       source: "component/template.hbs"
+ *     }],
+ *
+ *
+ *   },
+ *   stylesheets: {
+ *     "files": ["%cName%.css"]
+ *   },
+ *   layout: {
+ *     files: ["%cName.hbs%"],
+ *   },
+ *   example: {
+ *     files: ["%cName%.spec.js", "%cName%.js", "%cName%.hbs"],
+ *   }
+ * }
+ * 
+ *
+ */
+
 module.exports = {
   folderStructure: {
     components: {
-      /**
-       * You can specify sources for each file if it's different from the default value.
-       * Default value would be the singularized string of the folderStructure key.
-       *
-       * For example: component
-       *
-       * If no source could be found an empty file will be created.
-       */
       files: [{
         file: "%cName%.spec.js",
-        source: "component"
+        source: "component/template.spec.js"
       }, {
         file: "%cName%.js",
-        source: "component"
+        source: "component/template.js"
       }, {
         file: "%cName%.hbs",
-        source: "component"
+        source: "component/template.hbs"
       }],
 
 
